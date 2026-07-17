3af209fbad7b49912a1d472289482e45f3bafca9
DEBANJAN GHORUI <84003519+debanjan100@users.noreply.github.com>
2026-07-18 00:43:57 +0530

    Time: 3 ms (81.96%) | Memory: 20.4 MB (89.37%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key=lambda x: 0 if x != 0 else 1)
